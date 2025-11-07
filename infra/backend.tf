a948cd4575c21b348a1fbf836d2e647d55c9900a
Rafael Zucarelli <zucarelli83@gmail.com>
2025-11-07 10:45:20 -0300

    Lab - Terraform + Ansible com botão de Destroy

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,18 +1,8 @@
-RESOURCE_GROUP="rg-tfstate"
-STORAGE_ACCOUNT="tfstatecurso$RANDOM"
-CONTAINER_NAME="tfstate"
-LOCATION="eastus"
-
-az group create --name $RESOURCE_GROUP --location $LOCATION
-
-az storage account create \
-  --name $STORAGE_ACCOUNT \
-  --resource-group $RESOURCE_GROUP \
-  --location $LOCATION \
-  --sku Standard_LRS \
-  --encryption-services blob
-
-az storage container create \
-  --name $CONTAINER_NAME \
-  --account-name $STORAGE_ACCOUNT \
-  --auth-mode login
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatecurso10998"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
